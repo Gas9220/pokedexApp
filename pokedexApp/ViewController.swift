fc03958160614a9750214ea720dffc6e4866b09b
Gas9220 <gasparemonte@macbook-pro-di-gaspare.local>
2017-03-04 12:28:15 +0100

    Initial Commit

---
+++ pokedexApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedexApp
+//
+//  Created by Gaspare Monte on 04/03/17.
+//  Copyright © 2017 Gaspare Monte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
